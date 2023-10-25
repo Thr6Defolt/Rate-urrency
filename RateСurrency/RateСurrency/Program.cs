f43dbaeb511303e9870f0e3f56fc09d9add2d617
Thr6Defolt <43175754+Thr6Defolt@users.noreply.github.com>
2023-10-25 22:10:28 +0300

    Add files via upload

---
+++ RateСurrency/RateСurrency/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace RateСurrency
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RateCurrency<string> exchangeRateBoard = new RateCurrency<string>();
+
+            exchangeRateBoard.ChangeRate("27.5$");
+            exchangeRateBoard.ChangeRate("30.4$");
+            exchangeRateBoard.ChangeRate("29.7$");
+
+            exchangeRateBoard.PrintHistory();
+        }
+    }
+}
---
+++ RateСurrency/RateСurrency/RateCurrency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateСurrency
+{
+    internal class RateCurrency<T>
+    {
+        private List<T> rateHistory = new List<T>();
+        public void ChangeRate(T newRate)
+        {
+            rateHistory.Add(newRate);
+            Console.WriteLine($"New rate: {newRate}");
+        }
+
+        public void PrintHistory()
+        {
+            Console.WriteLine("Exchange Rate History:");
+            foreach (var rate in rateHistory)
+            {
+                Console.WriteLine(rate);
+            }
+        }
+    }
+}
